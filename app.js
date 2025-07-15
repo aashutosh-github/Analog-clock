c7584dc8d56ddd57900d4150f3af2ef05785b0eb
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-07-15 14:54:48 +0530

    Uploading files after initial work completion

---
+++ app.js
@@ -0,0 +1,18 @@
+const clockBody = document.querySelector("#clockBody");
+const numberDivs = document.querySelectorAll(".numbers");
+
+// subtracting the values as it provides some padding to the numbers inside the clock.
+const radius = clockBody.offsetWidth / 2 - 55;
+
+const centreX = clockBody.offsetWidth / 2;
+const centreY = clockBody.offsetHeight / 2;
+
+numberDivs.forEach((div, index) => {
+  // subtracting the 90 because the numbers start from horizontally 0 so subtracting 90 makes the numbers start from the top.
+  let angleRadians = (index * 30 - 90) * (Math.PI / 180);
+
+  let positionX = centreX + radius * Math.cos(angleRadians);
+  let positionY = centreY + radius * Math.sin(angleRadians);
+  div.style.left = positionX + "px";
+  div.style.top = positionY + "px";
+});
