a6e8c923895361d49a3d76bb7ab00126ab7c8f7e
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-07-16 21:13:13 +0530

    added the logic for rotation of hands

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const clockBody = document.querySelector("#clockBody");
 const numberDivs = document.querySelectorAll(".numbers");
+const hourHand = document.querySelector("#hourHand");
+const minuteHand = document.querySelector("#minuteHand");
+const secondHand = document.querySelector("#secondsHand");
 
 // subtracting the values as it provides some padding to the numbers inside the
 //  clock.
@@ -13,10 +16,31 @@ const centreY = clockBody.offsetHeight / 2 - 12;
 numberDivs.forEach((div, index) => {
   // subtracting the 90 because the numbers start from horizontally 0
   //  so subtracting 90 makes the numbers start from the top.
-  let angleRadians = (index * 30 - 90) * (Math.PI / 180);
+  const angleRadians = (index * 30 - 90) * (Math.PI / 180);
 
-  let positionX = centreX + radius * Math.cos(angleRadians);
-  let positionY = centreY + radius * Math.sin(angleRadians);
+  const positionX = centreX + radius * Math.cos(angleRadians);
+  const positionY = centreY + radius * Math.sin(angleRadians);
   div.style.left = positionX + "px";
   div.style.top = positionY + "px";
 });
+
+function clockTicking() {
+  const time = new Date();
+
+  const [hours, minutes, seconds] = [
+    time.getHours() % 12 || 12, //converts 24 hour format to 12 hour format
+    time.getMinutes(),
+    time.getSeconds(),
+  ];
+
+  const hourHandRotation = hours * 30 + minutes * 0.5;
+  const minuteHandRotation = minutes * 6 + seconds * 0.1;
+  const secondHandRotation = seconds * 6;
+
+  hourHand.style.transform = `rotate(${hourHandRotation}deg)`;
+  minuteHand.style.transform = `rotate(${minuteHandRotation}deg)`;
+  secondHand.style.transform = `rotate(${secondHandRotation}deg)`;
+}
+
+window.addEventListener("DOMContentLoaded", clockTicking);
+setInterval(clockTicking, 1000);
